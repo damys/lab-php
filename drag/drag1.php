5e7987ad37f703ed077fc905a68a8dd32ecad8d6
damys <damys@foxmail.com>
2018-05-22 09:03:57 +0800

    add drag1.php

---
+++ drag/drag1.php
@@ -0,0 +1,8 @@
+﻿<?php
+if($_FILES['file_upload']['error']==0){
+     $uploadFileName=$_FILES['file_upload']['name'];
+     $uploadFileTemp=$_FILES['file_upload']['tmp_name'];
+
+     $uploadFileTrue='./upload/';
+     move_uploaded_file($uploadFileTemp, $uploadFileTrue.$uploadFileName);
+ }
